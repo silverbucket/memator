daad78d3096b4d03b4a4e7f8f9b45ea51f913802
Nick Jennings <nick@silverbucket.net>
2024-03-01 14:40:30 +0100

    add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
 <h1>Welcome to SvelteKit</h1>
+<h2 class="text-3xl font-bold underline text-cyan-700">
+	Hello world!
+</h2>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
